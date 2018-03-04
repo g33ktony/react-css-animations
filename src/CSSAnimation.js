9fecf0b871ef90801ee160ea1264af031ae79c06
T0nyM <antonio@alchimia.mx>
2018-03-03 18:34:35 -0600

    fix: Change propTypes from react to propTypes new independent package

--- src/CSSAnimation.js
+++ src/CSSAnimation.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'propTypes'
 import prefixAll from 'inline-style-prefixer/static'
 
 const noop = () => {}
